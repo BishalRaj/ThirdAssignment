78197ff873d1fd62dd4180b02c7779951dd94482
BishalRaj <bisal1shakya@gmail.com>
2019-05-03 01:49:08 +0545

    register

--- app/src/main/java/com/example/thirdassignment/LoginFragment.java
+++ app/src/main/java/com/example/thirdassignment/LoginFragment.java
@@ -1,21 +1,70 @@
 package com.example.thirdassignment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
+    private EditText loginemail;
+    private EditText loginpassword;
+    private Button btnlogin;
+    private String un,pw;
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_login, container, false);
 
+            loginemail=view.findViewById(R.id.loginemail);
+            loginpassword=view.findViewById(R.id.loginpassword);
+            btnlogin=view.findViewById(R.id.btnlogin);
+            //add if remember me is checked or not..if checked do direct login...else need to login
+            preferences=this.getActivity().getSharedPreferences("APP_PREF", Context.MODE_PRIVATE);
+            editor=preferences.edit();
 
+                un=preferences.getString("email","");
+                pw=preferences.getString("password","");
 
+        System.out.println(un +" "+pw);
+
+            btnlogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(TextUtils.isEmpty(loginemail.getText().toString())){
+                        Toast.makeText(getActivity(), "Email cannot be empty", Toast.LENGTH_SHORT).show();
+                        loginemail.setText("");
+                        loginpassword.setText("");
+                    }else if(TextUtils.isEmpty(loginpassword.getText().toString())){
+                        Toast.makeText(getActivity(), "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                        loginpassword.setText("");
+                    }else{
+
+                        if (un.equals(loginemail.getText().toString())
+                                && pw.equals(loginpassword.getText().toString())){
+                            Toast.makeText(getActivity(), "Login Successful", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getActivity(), "Authentication Error", Toast.LENGTH_SHORT).show();
+                            loginemail.setText("");
+                            loginpassword.setText("");
+                        }
+
+                    }
+                    
+                }
+            });
 
 
         return view;
--- app/src/main/java/com/example/thirdassignment/RegisterFragment.java
+++ app/src/main/java/com/example/thirdassignment/RegisterFragment.java
@@ -1,19 +1,73 @@
 package com.example.thirdassignment;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
 
 public class RegisterFragment extends Fragment {
 
-
+    private EditText firstname,lastname,email,password;
+    private Button btnregister;
+    private String fn,ln,em,pwd;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_register, container, false);
+
+        firstname=view.findViewById(R.id.firstname);
+        lastname=view.findViewById(R.id.lastname);
+        email=view.findViewById(R.id.password);
+        password=view.findViewById(R.id.email);
+        btnregister=view.findViewById(R.id.btnregister);
+
+        preferences=this.getActivity().getSharedPreferences("APP_PRED", Context.MODE_PRIVATE);
+        editor=preferences.edit();
+
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fn=firstname.getText().toString();
+                ln=lastname.getText().toString();
+                em=email.getText().toString();
+                pwd=password.getText().toString();
+
+                if (fn.isEmpty()){
+                    Toast.makeText(getActivity(), "First Name cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if (ln.isEmpty()){
+                    Toast.makeText(getActivity(), "Last Name cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if (em.isEmpty()){
+                    Toast.makeText(getActivity(), "Email cannot be empty", Toast.LENGTH_SHORT).show();
+                }else if (pwd.isEmpty()){
+                    Toast.makeText(getActivity(), "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                }else{
+                    editor.putString("firstName",fn);
+                    editor.putString("lastName",ln);
+                    editor.putString("email",em);
+                    editor.putString("password",pwd);
+                    editor.commit();
+                    Toast.makeText(getActivity(), "You are successfully registered.", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+
+
         return view;
     }
 
